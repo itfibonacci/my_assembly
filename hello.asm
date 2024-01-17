061622c97dfa62d4f75a40ded59c186169a71589
Ubuntu <azureuser@kenthompson.kefvih5llcfujhchgconzkevtd.bx.internal.cloudapp.net>
2024-01-17 21:02:58 +0000

    hello world assembly

---
+++ hello.asm
@@ -0,0 +1,19 @@
+format ELF64 executable
+
+segment readable executable
+entry main
+main:
+	; places the syscall number 1 (write syscall) into the rax registry
+	mov rax, 1
+	; places the stdout fd to the rdi, which is the first parameter to the write syscall
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 14
+	syscall
+	
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+segment readable writeable
+msg db "Hello, World!", 10
