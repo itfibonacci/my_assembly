668b51e9df2f64fe41e86a8fcd7344a19444383c
Levon Tumanyan <levon.tumanyan@outlook.com>
2024-01-29 22:34:25 +0000

    how to loop

---
+++ looping/loop_with_jump.asm
@@ -0,0 +1,19 @@
+format ELF64 executable
+
+include "../include/macros.inc"
+
+segment readable executable
+
+entry main
+main:
+	mov r8, 10
+print_loop:
+	write STD_OUT, msg, msg_len
+	dec r8
+	test r8, r8
+	jnz print_loop
+end_program:
+	exit 0
+
+msg db "Hello", 10
+msg_len = $ - msg
